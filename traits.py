4625759a8fc0764de7a0afde7f178b79c2912798
Alex Staninger <alex.staninger@gmail.com>
2017-10-22 02:08:34 -0700

    typed out the phenotypes

--- traits.py
+++ traits.py
@@ -1,16 +1,16 @@
 traits = {}
 traits['romance'] = [
-    'eye-color', 'weight', 'bmi', 'red-hair', 'black-hair', 'skin-pigmentation'
+    'eye-color', 'weight', 'bmi', 'red-hair', 'black-hair', 'skin-pigmentation', 'male-pattern-baldness', 'freckles', 'red-wine-liking', 'white-wine-liking', 'endurance-performance'
 ]
 traits['p-health'] = [
-    
+    'lung-cancer', 'colorectal-cancer', 'gastric-cencer', 'breast-cancer', 'liver-cancer', 'pancreatic-cancer', 'prostate-cancer', 'type-2-diabetes', 'myocardial-infarction'
 ]
 traits['m-health'] = [
-    
+    'agreeablness', 'neuroticism', 'extraversion', 'conscientiousness', 'openness', 'depression', 'anger', 'reward-dependence', 'harm-avoidance', 'gambling', 'novelty-seeking'
 ]
 traits['friend'] = [
-    
+    'beard-thickness', 'morning-person', 'red-hair', 'black-hair', 'motion-sickness', 'handedness', 'longevity', 'smell-sensitivity-for-malt', 'smoking-behavior', 'alcohol-drinking-behavior'
 ]
 traits['work'] = [
-    
+    'agreeablness', 'openness', 'reward-dependence', 'harm-avoidance', 'gambling', 'novelty-seeking', 'childhood-intelligence', 'hearing-function', 'word-reading-ability', 'reading-and-spelling-ability'
 ]
